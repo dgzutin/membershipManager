e2528e502b0ebcfcfb5e433f72c2e42e1445b750
Danilo Zutin <dgzutin@gmail.com>
2019-07-15 21:38:23 +0200

    allow more characters prosition in user db

--- src/App/Entity/User.php
+++ src/App/Entity/User.php
@@ -35,7 +35,7 @@ class User
     /** @Column(type="string", length=10, nullable=true) **/
     public $title;
 
-    /** @Column(type="string", length=50, nullable=true) **/
+    /** @Column(type="string", length=100, nullable=true) **/
     public $position;
 
     /** @Column(type="string", length=100, nullable=false) **/
